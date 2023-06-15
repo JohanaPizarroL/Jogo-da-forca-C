944cf880f9f284d60ad506d8b220c26a84409e89
Johana Pizarro <102596180+JohanaPizarroL@users.noreply.github.com>
2023-06-15 15:39:27 -0300

    Header file
    Protótipos das funções
    Declaração das funções

---
+++ forca.h
@@ -0,0 +1,12 @@
+//header file do arquivo forca.c
+
+void abertura();
+int jachutou(char letra);
+void chuta();
+void desenhaforca();
+void palavrasecreta(); //escolhe palavra
+int acertou();
+int enforcou();
+
+
+//declaracão dos protótipos das funções
